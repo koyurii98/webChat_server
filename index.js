b1136c1afd70f504454cb63543e95b61d9a4a244
koyuri <rmeodprp0856@naver.com>
2021-08-18 16:12:23 +0900

    1차 성공~~

--- index.js
+++ index.js
@@ -16,29 +16,26 @@ io.sockets.on('connection', function (socket) {
   const { chatID, name } = socket.handshake.query;
   
   socket.join(chatID);
-  socket.name = name;
-  socket.user_id= chatID;
-  console.log(io.engine.clientsCount); 
-  //새로운 유저 알림
+
   socket.on('newUser', function () {
-    console.log(name+'님이 접속하였습니다.')
-    //모든 소켓에 전송
     socket.emit('update', { type: 'connect', name: 'SERVER', message: name + '님이 접속하였습니다.'})
   })
 
 
   socket.on('send_message', (message) =>{
-    console.log(message);
-    //해당유저한테 채팅보냄
-    socket.to(message.receiverChatID).emit('receive_message', { message})
+    socket.to(message.receiverChatID).emit('receive_message', { 
+      content: message.content,
+      senderChatID: message.senderChatID, 
+      receiverChatID: message.receiverChatID,
+      time: message.time,
+    })
   })
 
   //종료
   socket.on('disconnect', function() {
     socket.online = true;
-    console.log(socket.name+'님이 나가셨습니다.');
+    socket.emit('update', { type: 'disconnect', name: 'SERVER', message: name + '님이 나가셨습니다.'});
     socket.leave(chatID);
-    socket.broadcast.emit('update', { type: 'discounnect', name: 'SERVER', message: socket.name + '님이 나가셨습니다.'});
   })
 })
 
