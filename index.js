9c966c7d66e108478c13e15c5eff66f20a4496f8
koyuri <rmeodprp0856@naver.com>
2021-08-18 15:38:35 +0900

    서버수정

--- index.js
+++ index.js
@@ -13,29 +13,31 @@ const io = socket(server,{
 });
 app.use(router);
 io.sockets.on('connection', function (socket) {
+  const { chatID, name } = socket.handshake.query;
   
+  socket.join(chatID);
+  socket.name = name;
+  socket.user_id= chatID;
+  console.log(io.engine.clientsCount); 
   //새로운 유저 알림
-  socket.on('newUser', function (name) {
+  socket.on('newUser', function () {
     console.log(name+'님이 접속하였습니다.')
-    //소켓에 이름저장
-    socket.name = name;
     //모든 소켓에 전송
-    io.sockets.emit('update', { type: 'connect', name: 'SERVER', message: name + '님이 접속하였습니다.'})
+    socket.emit('update', { type: 'connect', name: 'SERVER', message: name + '님이 접속하였습니다.'})
   })
 
-  //전송한 메세지 받기
-  socket.on('message', function (data) {
-    //보낸사람 이름 추가
-    data.name = socket.name ;
-    console.log(data);
 
-    //보낸사람을 제외하고 메세지 전송
-    socket.broadcast.emit('update', data);
+  socket.on('send_message', (message) =>{
+    console.log(message);
+    //해당유저한테 채팅보냄
+    socket.to(message.receiverChatID).emit('receive_message', { message})
   })
 
   //종료
   socket.on('disconnect', function() {
+    socket.online = true;
     console.log(socket.name+'님이 나가셨습니다.');
+    socket.leave(chatID);
     socket.broadcast.emit('update', { type: 'discounnect', name: 'SERVER', message: socket.name + '님이 나가셨습니다.'});
   })
 })
