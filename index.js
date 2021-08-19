41bcde9223489abecc8840c93c897c2f59ad5c12
koyuri <rmeodprp0856@naver.com>
2021-08-19 17:30:37 +0900

    어느정도 기능구현완료

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const socket = require('socket.io');
 const http = require('http');
@@ -11,16 +12,22 @@ const io = socket(server,{
     origin:"*"
   }
 });
+
+let rooms = [];
 app.use(router);
 io.sockets.on('connection', function (socket) {
-  const { chatID } = socket.handshake.query;
-  
+  const { chatID, to } = socket.handshake.query;
+
   socket.join(chatID);
-
+  if(rooms.length===500){
+    console.log("이미존재하는 채팅방");
+    return;
+  }else{
+    rooms=rooms.concat({_id:socket.id, member:[chatID,to]});
+  }
   socket.on('newUser', function (name) {
-    socket.emit('update', { type: 'connect', name: 'SERVER', message: name + '님이 접속하였습니다.'})
+    socket.emit('update', { type: 'connect', name: 'SERVER', message: name + '님이 접속하였습니다.',userName:to})
   })
-
 
   socket.on('send_message', (message) =>{
     socket.to(message.receiverChatID).emit('receive_message', { 
@@ -31,13 +38,7 @@ io.sockets.on('connection', function (socket) {
     })
   })
 
-  // //종료
-  // socket.on('disconnect', function(name) {
-  //   socket.online = true;
-  //   socket.broadcast.emit('update', { type: 'disconnect', name: 'SERVER', message: name + '님이 나가셨습니다.'});
-  // })
-
-  socket.on('exitRoom', function(name) {
+  socket.on('exitRoom'|| 'disconnect', function(name) {
     socket.leave(chatID);
     socket.broadcast.emit('update', { type: 'disconnect', name: 'SERVER', message: name + '님이 나가셨습니다.'});
   })
